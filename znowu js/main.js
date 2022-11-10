116438f3bc5c65885c9e95f152c3f9882a55f75c
Piotrek882 <114090308+Piotrek882@users.noreply.github.com>
2022-11-10 13:25:12 +0100

    js

--- znowu js/main.js
+++ znowu js/main.js
@@ -115,3 +115,133 @@
 // }else{
 //     console.log("nie wchodzisz");
 // }
+
+//za pomocą samych ifów przykład 13 - jak nie należy robić
+// const age = 20;
+
+// if(age <=9){
+//     console.log("jesteś dieckiem")
+// }
+// if(age >9 && age <= 18){
+//     console.log("jesteś nastolatkiem")
+// }
+// if(age > 18 && age <= 70){
+//     console.log("jesteś dorosłym")
+// }
+// if(age>70){
+//     console.log("jesteś doświadczony życiowo")
+// }
+
+//przykład 14
+// const age = 20;
+
+// if(age <=9 && age >=0 ){
+//     console.log("jesteś dieckiem");
+// }else if(age >9 && age <= 18){
+//     console.log("jesteś nastolatkiem");
+// }else if(age > 18 && age <= 70){
+//     console.log("jesteś dorosłym");
+// }else if(age>70){
+//     console.log("jesteś doświadczony życiowo");
+// }else {console.log("nie możesz mieć takiego wieku")}
+
+//przykład 15
+// const age = 20;
+
+// if(age <=9 && typeof age === "number" ){
+//     console.log("jesteś dieckiem");
+// }else if( age <= 18 && age === "number"){
+//     console.log("jesteś nastolatkiem");
+// }else if(age <= 70 && age === "number"){
+//     console.log("jesteś dorosłym");
+// }else if(age>70 && age === "number"){
+//     console.log("jesteś doświadczony życiowo");
+// }else {console.log("nie możesz mieć takiego wieku")}
+
+//przykład 16
+// const guestName = "Izabela";
+// const guestAge = 15;
+
+// if(guestName) {
+//     if(guestAge >= 16){
+//     console.log("Witaj w klubie " + guestName)
+// } else {
+//     console.log("No niestety " + guestName + " nie wejdziesz")
+// }} else {
+//     console.log("nie wiem, jak masz na imię i nie wejdzesz!")
+// }
+/////////////////////////
+// const guestName = "";
+// const guestAge = 26;
+
+// if(guestName =="") {
+//     if(guestAge >= 16){
+//     console.log("Witaj w klubie " + guestName)
+// } else {
+//     console.log("No niestety " + guestName + " nie wejdziesz")
+// }} else {
+//     console.log("nie wiem, jak masz na imię i nie wejdzesz!")
+// }
+
+//przykład 17
+
+// const dayOfTheWeek = "środa";
+// switch(dayOfTheWeek) {
+//     case "poniedziałek":
+//         console.log("dziś jest poniediałek");
+//         break;
+//     case "wtorek":
+//         console.log("dziś jest wtorek")
+//         break;
+//     case "środa":
+//         console.log("dziś jest środek")
+//         break;
+//     default:
+//         console.log("nie mam pojęcia jaki jest dziś dzień")
+//         break;
+// }
+
+// const dayOfTheWeek = "czwartek";
+// switch(dayOfTheWeek) {
+//     case "poniedziałek":
+//         console.log("dziś jest poniediałek");
+//         break;
+//     case "wtorek":
+//         console.log("dziś jest wtorek")
+//         break;
+//     case "środa":
+//         console.log("dziś jest środek")
+//         break;
+//     default:
+//         console.log("nie mam pojęcia jaki jest dziś dzień")
+//         break;
+// }
+
+////// przykład 18 operator warunkowy (potrójny/trójkoway)
+
+//a ? b : c;
+
+//czyli warunek ? wykonaj mnie, jeśli warunek true : wykonaj mnie, jeśli warunek false 
+
+// const score = 100;
+// 20 == "20" && score ? console.log("wykonaj się, bo prawda") : console.log("wykonaj się, jesli warunek nie jest prawdziwy")
+
+// false || true ? console.log ("prawda") : console.log ("fałsz");
+
+// const score = 100
+// score > 120 ? console.log ("prawda") : console.log ("fałsz");
+
+// 0 || 1 ? console.log ("prawda") : console.log ("fałsz");
+
+// console.log(1 > 10 ? 1 : 0);
+
+////// przykład 19
+
+// let gameResult = 0;
+// let killedMonsters = 11;
+
+// console.log(gameResult += killedMonsters > 10 ? 1 : 0;)
+
+//////// przykład 20
+// const playerName = "Warrior";
+// console.log(` Witaj ${playerName ? playerName : "wojowniku"}`);
