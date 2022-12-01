8575c32ecfdd247c6d9aa19ff1c90fa738e9b072
Piotrek882 <114090308+Piotrek882@users.noreply.github.com>
2022-12-01 07:55:40 +0100

    js znowu

---
+++ znowu js/folder/main.js
@@ -0,0 +1,7 @@
+let liczba = parseInt(prompt("podaj liczbę"));
+alert("Twoja liczba to " + liczba);
+const potega = parseInt(prompt("Do której potęgi chcesz podnieść swoją liczbę"));
+let wynik;
+wynik = liczba ** potega;
+alert("potęga twojej liczby to: " + wynik);
+let x = 1;
