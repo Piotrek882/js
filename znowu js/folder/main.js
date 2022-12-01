b69ee36bdaa8e423219496a685b78980929db157
Piotrek882 <114090308+Piotrek882@users.noreply.github.com>
2022-12-01 13:03:58 +0100

    petle1

--- znowu js/folder/main.js
+++ znowu js/folder/main.js
@@ -6,7 +6,22 @@ wynik = liczba ** potega;
 alert("potęga twojej liczby to: " + wynik);
 let x = 1;
 let wynik2 = x ** 2;
-while(wynik2 != liczba){
-    x++
-    wynik2 = x ** 2;
+let wynik3 = x ** 3;
+alert("Twoja liczba to " + liczba);
+let decyzja1 = parseInt(prompt("Co chcesz zrobić ze swoją liczbą? 1 - pierwiastek pierwszego stopnia, 2 - pierwiastek trzeciego stopnia"))
+if(decycja1 == 1){
+    while(wynik2 != liczba){
+        x++
+        wynik2 = x ** 2;
+    }
+
+    alert("Pierwiastek drugiego stopnia z twojej liczby to " + x);
+} else if (decyzja1 == 2) {
+    
+    while (wynik3 != liczba) {
+        x++
+        wynik3 = x ** 3;
+    }
+    
+    alert("Pierwiastek trzeciego stopnia z twojej liczby to " + x);
 }
