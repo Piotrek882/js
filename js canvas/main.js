da3f01b151869467830531df139dfbce7116b91b
Piotrek882 <114090308+Piotrek882@users.noreply.github.com>
2023-06-22 18:14:31 +0200

    Add files via upload

--- js canvas/main.js
+++ js canvas/main.js
@@ -1,119 +1,120 @@
-const speed = 5;
+let canvasWidth = document.body.clientWidth - 10;
+let canvasHeight = document.body.clientHeight - 10;
 const canvas = document.createElement('canvas');
-canvas.width = 1200;
-canvas.height = 600;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
 document.body.appendChild(canvas);
+let canvasCurrentHeight;
+let canvasCurrentWidth;
 
 const ctx = canvas.getContext('2d');
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 const square = {
-    x: 600,
-    y: 300,
-    size: 50,
-    color: '#1e88e5'
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 50,
+  color: '#1e88e5'
+};
+
+let enemy = {
+  x: 15 + getRandomInt(50),
+  y: 15 + getRandomInt(50),
+  size: 30,
+  color: '#f00'
 };
 
 let mouseX = 0;
 let mouseY = 0;
+const speed = 7;
 
 function drawSquare() {
-    ctx.fillStyle = square.color;
-    ctx.fillRect(square.x, square.y, square.size, square.size);
+  ctx.fillStyle = square.color;
+  ctx.fillRect(square.x - square.size / 2, square.y - square.size / 2, square.size, square.size);
 }
 
-function drawAim() {
-    ctx.beginPath();
-    ctx.moveTo(square.x + 25, square.y + 25);
-    ctx.lineTo(mouseX, mouseY);
-    ctx.moveTo(square.x + 25, square.y + 25);
-    ctx.strokeStyle = 'rgba(0, 255, 0, 0.3)';
-    ctx.lineWidth = 10;
-    ctx.stroke();
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
+
+// let enemyX = getRandomInt(1500);
+// let enemyY = getRandomInt(600);
+// 
+function drawEnemy() {
+  ctx.fillStyle = '#f00';
+  ctx.fillRect(enemy.x, enemy.y, enemy.size, enemy.size);
+  }
+  
+  function drawAim() {
+    ctx.beginPath();
+    ctx.moveTo(square.x, square.y);
+    ctx.lineTo(mouseX, mouseY);
+    ctx.moveTo(square.x, square.y);
+    ctx.strokeStyle = '#f00';
+    ctx.lineWidth = 3;
+  ctx.stroke();
+}
+let step = 0;
 
 function update() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (keys['a'] && square.x >= 0) {
-        square.x -= speed;
-    } else if (keys['d'] && square.x + 50 <= canvas.width) {
-        square.x += speed;
-    }
-
-    if (keys['w'] && square.y >= 0) {
-        square.y -= speed;
-    } else if (keys['s'] && square.y + 50 <= canvas.height) {
-        square.y += speed;
-    }
-
-    drawSquare();
-    drawEnemy();
-    drawAim();
-
-    requestAnimationFrame(update);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  //   const dx = mouseX - square.x;
+  //   const dy = mouseY - square.y;
+  //   const angle = Math.atan2(dy, dx);
+  //   square.x += Math.cos(angle) * speed;
+  //   square.y += Math.sin(angle) * speed;
+  
+  if (keys['a'] && square.x > 0+25) {
+    square.x -= speed;
+  } else if (keys['d'] && square.x < canvas.width-25) {
+    square.x += speed;
+  }
+  
+  if (keys['w'] && square.y > 0+25) {
+    square.y -= speed;
+  } else if (keys['s'] && square.y < canvas.height-25) {
+    square.y += speed;
+  }
+  if(step < 100){
+    enemy.x += 1;
+    step +=1;
+  } else if(step < 300){
+    enemy.y +=2;
+    step +=1;
+  } else if(step < 500){
+    enemy.x +=1;
+    enemy.y -=1;
+    step +=1;
+  } else if(step < 700){
+    enemy.x += 1;
+    enemy.y += 0.5;
+  }
+  
+  drawSquare();
+  drawAim();
+  drawEnemy();
+  
+  requestAnimationFrame(update);
 }
 
 function onMouseMove(e) {
-    mouseX = e.clientX - canvas.offsetLeft;
-    mouseY = e.clientY - canvas.offsetTop;
+  mouseX = e.clientX - canvas.offsetLeft;
+  mouseY = e.clientY - canvas.offsetTop;
 }
 
 function onKeyDown(e) {
-    keys[e.key] = true;
+  keys[e.key] = true;
 }
 
 function onKeyUp(e) {
-    keys[e.key] = false;
+  keys[e.key] = false;
 }
 
 const keys = {};
-
-let step = 0;
-
-const enemy = {
-    x: getRandomInt(50),
-    y: getRandomInt(50),
-    size: 30,
-    color: 'red'
-};
-
-function enemyMovement() {
-    if (step < 100) {
-        enemy.x += 2;
-        enemy.y++;
-        step++
-    } else if (step < 200) {
-        enemy.y += 2;
-        step++;
-    } else if (step < 300) {
-        enemy.x += getRandomInt(4);
-        step++;
-    } else if (step < 400) {
-        enemy.y -= getRandomInt(2);
-        enemy.x += getRandomInt(2);
-        step++;
-    } else if (step < 500) {
-        enemy.x -= getRandomInt(2);
-        step++;
-    } else if (step < 600) {
-        enemy.x += getRandomInt(3);
-        enemy.y += getRandomInt(2);
-        step++;
-    } else if (step < 700) {
-        enemy.x += getRandomInt(4);
-        step++;
-    }
-}
-setInterval(enemyMovement, 10); 77
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-function drawEnemy() {
-    ctx.fillStyle = enemy.color;
-    ctx.fillRect(enemy.x, enemy.y, enemy.size, enemy.size);
-}
 
 canvas.addEventListener('mousemove', onMouseMove);
 window.addEventListener('keydown', onKeyDown);
@@ -121,25 +122,32 @@ window.addEventListener('keyup', onKeyUp);
 
 update();
 
-function limitEnemyMovement() {
-    if (enemy.x <= 0) {
-        enemy.x = 0;
-    }
-    if (enemy.x + enemy.size >= canvas.width) {
-        enemy.x = canvas.width - enemy.size;
-    }
-    if (enemy.y <= 0) {
-        enemy.y = 0;
-    }
-    if (enemy.y + enemy.size >= canvas.height) {
-        enemy.y = canvas.height - enemy.size;
-    }
-}
-setInterval(limitEnemyMovement, 10);
+document.addEventListener('contextmenu', function(event) {
+  event.preventDefault();
+});
 
-function hitCheck() {
-    if (enemy.x + 30 >= square.x && enemy.x <= square.x + 50 && enemy.y + 30 >= square.y && enemy.y <= square.y + 50) {
-        alert('HIT!!');
-    }
+function checkPlayer(){
+  canvasCurrentHeight = canvas.getAttribute('height');
+  canvasCurrentWidth = canvas.getAttribute('width');
+  if(square.x > canvasCurrentWidth){
+    square.x = canvasCurrentWidth - 25;
+  }
+  if(square.y > canvasCurrentHeight){
+    square.y = canvasCurrentHeight - 25;
+  }
+  if(enemy.x > canvasCurrentWidth - 30){
+    enemy.x -= 1;
+  }
+  if(enemy.y > canvasCurrentHeight - 30){
+    enemy.y -= 1;
+  }
+
 }
-setInterval(hitCheck, 1);
+// function breakCheck(){
+//   if(
+    
+//   ){
+//     alert('zderzenie');
+//   }
+// }
+// setInterval(breakCheck, 0.1);
